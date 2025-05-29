6f3616d0aadb15879e995f33d628afdada296bb9
Kuba Hodi <jakhod2007@gmail.com>
2025-05-29 18:56:46 +0200

    User and admin functionality WIP + better view and register form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get "admin" => "admin#index"
+  controller :sessions do
+    get "login" => :new
+    post "login" => :create
+    delete "logout " => :destroy
+  end
+
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
+  resources :users
   resources :cars
   resources :brands
   root "home#index"
@@ -7,7 +18,6 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_26_184220) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_29_163612) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -35,6 +35,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_26_184220) do
     t.datetime "updated_at", null: false
     t.bigint "brand_id", null: false
     t.index ["brand_id"], name: "index_cars_on_brand_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "cars", "brands"
